dce974444c86b790dad3bbd1f73f06db7843e6ef
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-05-21 07:39:51 -0700

    Update seed with flight starts and durations

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,26 +26,38 @@ Airport.create!([{
 Flight.create!([{
 	departure_airport_id: 1,
 	arrival_airport_id: 2,
+	start: "2024-07-01 06:30",
+	flight_duration: 660,
 },
 {
 	departure_airport_id: 2,
 	arrival_airport_id: 1,
+	start: "2024-07-02 14:10",
+	flight_duration: 640,
 },
 {
 	departure_airport_id: 3,
 	arrival_airport_id: 2,
+	start: "2024-07-04 19:40",
+	flight_duration: 380,
 },
 {
 	departure_airport_id: 2,
 	arrival_airport_id: 3,
+	start: "2024-07-07 03:15",
+	flight_duration: 395,
 },
 {
 	departure_airport_id: 1,
 	arrival_airport_id: 3,
+	start: "2024-07-09 05:20",
+	flight_duration: 330,
 },
 {
 	departure_airport_id: 3,
 	arrival_airport_id: 1,
+	start: "2024-07-14 15:30",
+	flight_duration: 290,
 }
 ])
 
