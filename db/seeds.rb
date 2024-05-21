af46d3478bc591c99ef56739a3be19e2625a10a5
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-05-21 06:55:53 -0700

    Create seed for airports and flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,47 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+Airport.destroy_all
+Flight.destroy_all
+
+Airport.create!([{
+	code: "LAX"
+},
+{
+	code: "LHR"
+},
+{
+	code: "JFK"
+}
+])
+
+Flight.create!([{
+	departure_airport_id: 1,
+	arrival_airport_id: 2,
+},
+{
+	departure_airport_id: 2,
+	arrival_airport_id: 1,
+},
+{
+	departure_airport_id: 3,
+	arrival_airport_id: 2,
+},
+{
+	departure_airport_id: 2,
+	arrival_airport_id: 3,
+},
+{
+	departure_airport_id: 1,
+	arrival_airport_id: 3,
+},
+{
+	departure_airport_id: 3,
+	arrival_airport_id: 1,
+}
+])
+
+p "Created #{Airport.count} airports"
+p "Created #{Flight.count} flights"
