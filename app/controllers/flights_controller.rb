eaf588a1bcda33c97a92f6f74543f60b096d5665
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-05-21 09:07:37 -0700

    Add flights view and update flight controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,7 @@
 class FlightsController < ApplicationController
 
 	def index
+		@airport_options = Airport.all.map { |f| [f.code, f.id] }
+		@start_date = Flight.all.map { |f| [f.start.strftime("%m-%d-%Y %H:%M %p"), f.id] }
 	end
 end
