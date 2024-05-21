21dff6aa5a1d0d19852c5313e728cbac3f86764e
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-05-21 07:39:28 -0700

    Change flight_duration db type to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_21_023710) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_21_143221) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_21_023710) do
     t.integer "departure_airport_id"
     t.integer "arrival_airport_id"
     t.datetime "start"
-    t.time "flight_duration"
+    t.integer "flight_duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
