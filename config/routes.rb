5b75e527de2c54459a82345fd382235d9413d712
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-05-22 10:32:22 -0700

    Update booking routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
 
   root "flights#index"
 
-  resources :flights, only:[:index, :new]
+  resources :flights, only:[:index]
   resources :bookings, only:[:index, :new, :create]
 end
