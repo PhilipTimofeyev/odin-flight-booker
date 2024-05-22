54b4e4b8da58831a307a2ea28e2562260e11f68e
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-05-22 09:41:13 -0700

    Add strong params to flight controller and update passenger number submission

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,23 +1,24 @@
 class FlightsController < ApplicationController
 
 	def index
+		# @flights = Flight.all
 		@airport_options = Airport.all.map { |f| [f.code, f.id] }
 		@flight_date = Flight.all.map { |f| [f.date, f.date] }.uniq
 
-		if search_params.present?
+		if params[:commit]
 			@flights = Flight.where(filter_empty_params).to_a
-			@hmm = params
+			@passengers = passenger_num_params
 		end
-	end
-
-	def new
-		@select_flight = params
 	end
 
 	private
 
 	def search_params
-		params.permit(:departure_airport, :arrival_airport, :date)
+		params.require(:flight).permit(:departure_airport, :arrival_airport, :date)
+	end
+
+	def passenger_num_params
+		params.require(:flight).permit(:num_of_passengers)
 	end
 
 	def filter_empty_params
