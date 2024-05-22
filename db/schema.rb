f6e024ebc01d870b0a420f3b1f75f74b1cd4dbf4
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-05-22 07:42:36 -0700

    Add Passenger model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_22_045805) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_22_143823) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
@@ -30,6 +30,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_22_045805) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.time "time"
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
