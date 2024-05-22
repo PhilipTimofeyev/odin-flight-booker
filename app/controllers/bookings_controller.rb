3c7e70e91b54f51bd70a0662f0fe06fba38d3f68
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-05-22 10:32:46 -0700

    Add table to display flight selected flight info

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,9 @@
 class BookingsController < ApplicationController
 	def new
-		@booking = Booking.new(booking_params)
+		# @booking = Booking.new(booking_params)
+		@booking = Booking.new
+		@flight = Flight.all.find(booking_params[:flight_id])
+		@num_of_passengers = booking_params[:num_of_passengers]
 	end
 end
 
@@ -10,5 +13,5 @@ end
 private
 
 def booking_params
-	params.require(:flight).permit(:selected_flight)
+	params.require(:booking).permit(:flight_id, :num_of_passengers)
 end
