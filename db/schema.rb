da3841f59e5f6a7685749f8c609eaf5202fff370
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-05-22 10:46:10 -0700

    Add booking_id to Passenger model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_22_143823) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_22_174318) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_22_143823) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "booking_id"
   end
 
 end
