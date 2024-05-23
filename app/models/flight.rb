7281d677e12e59307e16825145d380be3605d844
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-05-23 07:39:00 -0700

    Update association so passengers of a flight can be returned through Flight.passengers

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,4 +2,5 @@ class Flight < ApplicationRecord
 	belongs_to :departure_airport, class_name: "Airport", optional: true
 	belongs_to :arrival_airport, class_name: "Airport", optional: true
 	has_many :bookings
+	has_many :passengers, through: :bookings
 end
