7fbcdd5219f3f6065e3fe67857abccf2e54df40f
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-05-31 18:13:38 -0700

    Fix dynamic adding of passengers

--- app/javascript/controllers/passenger_controller.js
+++ app/javascript/controllers/passenger_controller.js
@@ -1,10 +1,30 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ 'temp', 'target', 'parent']
+  static targets = [ 'temp', 'target', 'parent', 'id']
+  static values = { count: 1}
+
   add() {
-    let temp = this.tempTarget
-    let clon = temp.content.cloneNode(true);
-    this.targetTarget.appendChild(clon)
+    let temp = this.tempTarget.content.firstChild.nextSibling;
+    let tempClone = temp.cloneNode(true);
+
+    this.countValue++
+
+    let newId = this.countValue
+
+    let toReplaceEmailname = tempClone.lastElementChild.name
+    tempClone.lastElementChild.name = toReplaceEmailname.replace( /\d/g, newId)
+
+    let toReplaceNameName = tempClone.firstElementChild.nextSibling.nextSibling.name
+    tempClone.firstElementChild.nextSibling.nextSibling.name = toReplaceNameName.replace( /\d/g, newId)
+
+    this.parentTarget.appendChild(tempClone)
+  }
+
+  remove() {
+    let last = this.targetTarget.firstChild.nextSibling
+    console.log(last)
+    // let clon = temp.content.cloneNode(true);
+    last.remove()
   }
 }
