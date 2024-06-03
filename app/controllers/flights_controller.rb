ca3aee4cfcd52e9cefb057ff9507fa8fb45c1fc5
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-06-03 10:55:13 -0700

    Add method to add airports from API call to the database as Airport objects

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,37 +1,11 @@
 class FlightsController < ApplicationController
 
-	OPTIONS = [ "RCTP",
-						 "SAEZ",
-						 "EDDF",
-						 "EGSH",
-						 "EGCC",
-						 "LIMC",
-						 "LGAV",
-						 "EHAM",
-						 "EDDL",
-						 "EDDH",
-						 "LDZA",
-						 "LJLJ",
-						 "ENGK",
-						 "LTFM",
-						 "LIRF",
-						 "RJTT",]
-
 	def index
-		# @airport_options = Airport.all.map { |f| [f.code, f.id] }
-		# @airport_options = OPTIONS
-		# @flight_date = Flight.all.map { |f| [f.date, f.date] }.uniq
 
 		if params[:date].present?
-			# @passengers = passenger_num_params
-			# Rails.cache.write("list",[1,2,3])
-
-			# get_flights
-
 			get_all_flights
-
+			add_airports
 			get_airports
-			# get_airport_non_api
 
 			# debugger
 		end
@@ -45,41 +19,46 @@ class FlightsController < ApplicationController
 
 	end
 
-		def get_airport_non_api
-			@airport_depart = OPTIONS
-			@airport_arrive = OPTIONS
+	def get_all_flights
+		# debugger
+		flight_date = date_params[:date]
+		flight_date_start = convert_to_unix(flight_date)
+		flight_date_end = (Date.parse(flight_date).to_time + 2.hours).to_i
+
+		# get_request = "https://opensky-network.org/api/flights/all?begin=#{flight_date_start}&end=#{flight_date_end}"
+
+		response = RestClient.get("https://opensky-network.org/api/flights/all?begin=#{flight_date_start}&end=#{flight_date_end}")
+		# response = RestClient.get("https://opensky-network.org/api/flights/all?begin=#{flight_date_start}&end=#{flight_date_end}")
+		@json = JSON.parse(response).uniq
+
+		Rails.cache.write("json_flights", @json)
+	end
+
+	def get_airports
+		# departing = Airport.all
+		@airport_depart = Airport.all.map {|airport| airport.code}.sort
+		@airport_arrive = Airport.all.map {|airport| airport.code}.sort
+	end
+
+	def get_date_flights
+		arrival_airport = search_params[:arrival_airport]
+		departure_airport = search_params[:departure_airport]
+		Rails.cache.read("json_flights").select {|flight| flight["estArrivalAirport"] == arrival_airport && flight["estDepartureAirport"] == departure_airport}
+
+		# debugger
+	end
+
+	def add_airports
+		@json.each do |flight| 
+			unless Airport.all.exists?(code: flight['estDepartureAirport'])
+				Airport.create(code: flight['estDepartureAirport']) unless (flight['estDepartureAirport']).nil?
+			end
 		end
+	end
 
-		def get_all_flights
-			# debugger
-			flight_date = date_params[:date]
-			flight_date_start = convert_to_unix(flight_date)
-			flight_date_end = (Date.parse(flight_date).to_time + 2.hours).to_i
-
-			# get_request = "https://opensky-network.org/api/flights/all?begin=#{flight_date_start}&end=#{flight_date_end}"
-
-			response = RestClient.get("https://opensky-network.org/api/flights/all?begin=#{flight_date_start}&end=#{flight_date_end}")
-			# response = RestClient.get("https://opensky-network.org/api/flights/all?begin=#{flight_date_start}&end=#{flight_date_end}")
-			@json = JSON.parse(response).uniq
-			Rails.cache.write("json_flights", @json)
-		end
-
-		def get_airports
-			@airport_depart = @json.map {|hsh| hsh["estDepartureAirport"]}.compact.uniq.sort
-			@airport_arrive = @json.map {|hsh| hsh["estArrivalAirport"]}.compact.uniq.sort
-		end
-
-		def get_date_flights
-			arrival_airport = search_params[:arrival_airport]
-			departure_airport = search_params[:departure_airport]
-			Rails.cache.read("json_flights").select {|flight| flight["estArrivalAirport"] == arrival_airport && flight["estDepartureAirport"] == departure_airport}
-
-			# debugger
-		end
-
-		def convert_to_unix(date)
-			Date.parse(date).to_time.to_i
-		end
+	def convert_to_unix(date)
+		Date.parse(date).to_time.to_i
+	end
 
 	private
 
